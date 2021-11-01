5b467e6c76e7b8ac61e74329131171caaca6cf58
JoseLValdes <jolvaldgo@gmail.com>
2021-11-01 15:35:15 +0100

    Functions: Html image Html Dropzone Refactor buttonStart

--- scripts/game.js
+++ scripts/game.js
@@ -5,23 +5,36 @@ const cardArray = cards.map((card) => card);
 const buttonStart = document.getElementById("start");
 const gameLine = document.getElementById("game-line");
 
-
-
 /******FUNTIONS*********/
+
 function getRandomCard(cardArray) {
   return cardArray[Math.floor(Math.random() * cardArray.length)];
 }
 
+function getHtmlImgElement(card) {
+  let image = document.createElement("img");
+  image.setAttribute("class", "card");
+  image.setAttribute("id",card.year);
+  image.setAttribute("src", card.imageBack);
+  return image;
+}
+
+function getHtmlDropZone() {
+  const newDropZone = document.createElement("div");
+  newDropZone.setAttribute("class", "dropzone");
+  return newDropZone;
+}
+
 buttonStart.addEventListener(
   "click",
+
   function () {
-    const initialCard =  document.createElement("div");
-    let spaceAfter = document.createElement("div");
-    let spaceBefore = document.createElement("div");
-    gameLine.appendChild(spaceBefore).setAttribute("class", "dropzone");
-    gameLine.appendChild(initialCard).setAttribute("class","card");
-    gameLine.appendChild(spaceAfter).setAttribute("class", "dropzone");
-    console.log(getRandomCard(cards));
+    gameLine.appendChild(getHtmlImgElement(getRandomCard(cardArray)));
+    setTimeout(function () {
+      gameLine.insertBefore(getHtmlDropZone(), gameLine.childNodes[0]);
+      gameLine.appendChild(getHtmlDropZone());
+    }, 1000);
+    console.log(getRandomCard(cards).imageBack);
   },
   false
 );
