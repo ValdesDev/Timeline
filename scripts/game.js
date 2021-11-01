486b9915cc0f9685f1a78b350fa28916bb79ec90
JoseLValdes <jolvaldgo@gmail.com>
2021-11-01 11:46:09 +0100

    Button Start blank elements

--- scripts/game.js
+++ scripts/game.js
@@ -1,18 +1,27 @@
 import { cards } from "./cards.js";
 const cardArray = cards.map((card) => card);
 
-/* DOM HTML ELEMENTS*/
-let buttonStart = document.getElementById("start");
+/*****DOM HTML ELEMENTS*******/
+const buttonStart = document.getElementById("start");
+const gameLine = document.getElementById("game-line");
 
+
+
+/******FUNTIONS*********/
 function getRandomCard(cardArray) {
-  return cardArray[Math.floor(Math.random() * (cardArray.length))];
+  return cardArray[Math.floor(Math.random() * cardArray.length)];
 }
 
 buttonStart.addEventListener(
   "click",
   function () {
+    const initialCard =  document.createElement("div");
+    let spaceAfter = document.createElement("div");
+    let spaceBefore = document.createElement("div");
+    gameLine.appendChild(spaceBefore).setAttribute("class", "dropzone");
+    gameLine.appendChild(initialCard).setAttribute("class","card");
+    gameLine.appendChild(spaceAfter).setAttribute("class", "dropzone");
     console.log(getRandomCard(cards));
   },
-
   false
 );
