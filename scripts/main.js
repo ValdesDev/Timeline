02a4a4035cf8b5805c1d25300fbf343777119dcf
JoseLValdes <jolvaldgo@gmail.com>
2021-11-03 20:16:01 +0100

    button style

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,12 @@ export const buttonStart = document.getElementById("start");
 export const buttonRules = document.getElementById("rules");
 export const buttonReset = document.getElementById("reset");
 export const buttonBack = document.getElementById("back");
-export const ruleBox = document.getElementById("rules-box");
-export const score = document.getElementById("score");
+export const buttonAgain = document.getElementById("again");
 
+export const ruleBox = document.getElementById("rules-box");
+export const endGame = document.getElementById("end-game");
+export const score = document.getElementById("score");
+export const liveBox = document.getElementsByClassName("live-box");
+export const lives = document.getElementsByClassName("live");
+export const feedback = document.getElementById("feedback");
 
