151e1791ed7aa927bf7ed49627b2a9301cc1f290
JoseLValdes <jolvaldgo@gmail.com>
2021-11-04 18:46:03 +0100

    Problemas finales 2

---
+++ scripts/cards copy.js
@@ -0,0 +1,199 @@
+export let cards = [
+
+  {
+    year: -133,
+    name: "Asedio de numancia por los Romanos"
+  },
+  {
+    year: -25,
+    name: "Roma somete toda la peninsula, tras las guerras cantabras"
+  },
+  {
+    year: 64,
+    name: "Persecución de los cristianos por Nerón"
+  },
+  {
+    year: 98,
+    name: "Trajano, Cesar Hispano"
+  },
+  {
+    year: 410,
+    name: "Comienza la Conquista Visigoda de Hispania"
+  },
+  {
+    year: 448,
+    name: "Requiario rey de los suevos, invade Hispania"
+  },
+  {
+    year: 452,
+    name: "Genserico rey de los vandalos invade la betica"
+  },
+  {
+    year: 587,
+    name: "Recaeredo instala el Catolicismo"
+  },
+  {
+    year: 630,
+    name: "Los Visigodos expulsa a los bizantinos (ultimos Romanos)"
+  },
+  {
+    year: 711,
+    name: "Los Musulmanes llegan a Gibraltar"
+  },
+  {
+    year: 722,
+    name: "Batalla de Covadonga"
+  },
+  {
+    year: 750,
+    name: "Alfonso 1 rey de asturias"
+  },
+  {
+    year: 756,
+    name: "Se crea el Emirato de Cordoba"
+  },
+  {
+    year: 778,
+    name: "Batalla de roncesvalle"
+  },
+  {
+    year: 780,
+    name: "Mezquita de Cordoba"
+  },
+  {
+    year: 910,
+    name: "Reino de leon"
+  },
+  {
+    year: 929,
+    name: "Se crea el Califato de cordoba"
+  },
+  {
+    year: 936,
+    name: "Construcción de Medina azahara"
+  },
+  {
+    year: 1035,
+    name: "Reino de Aragon"
+  },
+  {
+    year: 1038,
+    name: "Reino de castilla"
+  },
+  {
+    year: 1085,
+    name: "Toledo se rinde ante Castilla"
+  },
+  {
+    year: 1172,
+    name: "Comienza la construccion de la Giralda"
+  },
+  {
+    year: 1212,
+    name: "Las Navas de tolosa"
+  },
+  {
+    year: 1238,
+    name: "Se funda el Reino Nazari"
+  },
+  {
+    year: 1265,
+    name: "Alfonso X el Sabio y el explendor de Toledo"
+  },
+  {
+    year: 1469,
+    name: "Se casan los Reyes Catolicos"
+  },
+  {
+    year: 1519,
+    name: "Carlos I Creacion del Sacro Imperio Español"
+  },
+  
+  {
+    year: 1605,
+    name: "El Quijote"
+  },
+  {
+    year: 1656,
+    name: "Velazquez pinta las Meninas"
+  },
+  {
+    year: "1713dc",
+    name: "Tratado de Utrecht y la Ley Sadica"
+  },
+  
+  {
+    year: 1763,
+    name: "Se crea la Loteria Nacional"
+  },
+  {
+    year: 1808,
+    name: "Motin de aranjuez, Guerra de independencia"
+  },
+  {
+    year: 1820,
+    name: "Comienza el Trienio Liberal"
+  },
+  {
+    year: 1823,
+    name: "Destierro de los intelectuales y cierre de universidades"
+  },
+  {
+    year: 1835,
+    name: "Mendizabal desamortiza los bienes de la iglesia"
+  },
+  {
+    year: "1849dc",
+    name: "Alumbrado de Gas en Madrid"
+  },
+  {
+    year: "1856dc",
+    name: "Se inaugura el teatro de la zarzuela"
+  },
+  {
+    year: 1888,
+    name: "Fundación de la UGT y el PSOE"
+  },
+  {
+    year: "1893dc",
+    name: "Atentado Anarquista en el Liceo de BCN"
+  },
+  {
+    year: 1904,
+    name: "Descanso de los domingos para los obreros"
+  },
+  {
+    year: 1909,
+    name: "La Semana Trágica",
+    image:"images/cards/tragB.png",
+    imageBack:"images/cards/tragA.png",
+  },
+  {
+    year: 1912,
+    name: "Asesinato de Canalejas"
+  },
+  {
+    year: 1921,
+    name: "Desastre de Annual (Marruecos)",
+    image:"images/cards/annualB.png",
+    imageBack:"images/cards/annualA.png",
+  },
+  {
+    year: 1923,
+    name: "Golpe de Estado de Miguel Primo de Rivera"
+  },
+  {
+    year: 1973,
+    name: "Asesinato de Carrero Blanco"
+  },
+  {
+    year: 1981,
+    name: "Intento de golpe de estado"
+  },
+  {
+    year: 2011,
+    name: "ETA anuncia cese de lucha armada y el 15M",
+    image:
+    imageBack:
+  }
+];
--- scripts/cards.js
+++ scripts/cards.js
@@ -3,244 +3,244 @@ export let cards = [
     name: "Primera República Española",
     year: 1873,
     image: "images/cards/1repB.png",
-    imageBack: "images/cards/1repA.png",
+    imageBack: "images/cards/1repA.png"
   },
   {
     name: "Fusilamientos en la Moncola",
     year: 1808,
     image: "images/cards/3mayB.png",
-    imageBack: "images/cards/3mayA.png",
+    imageBack: "images/cards/3mayA.png"
   },
   {
     name: "Construcción del acueducto de Segovia",
     year: -116,
     image: "images/cards/AcueductoB.png",
-    imageBack: "images/cards/AcueductoA.png",
+    imageBack: "images/cards/AcueductoA.png"
   },
   {
     name: "Hispania se convierte en Al Ándalus",
     year: 716,
     image: "images/cards/AlanB.png",
-    imageBack: "images/cards/AlanA.png",
+    imageBack: "images/cards/AlanA.png"
   },
   {
     name: "Comienza el Ducado de Alba",
     year: 1472,
     image: "images/cards/albaB.png",
-    imageBack: "images/cards/albaA.png",
+    imageBack: "images/cards/albaA.png"
   },
   {
     name: "Los Almoravides invaden Al Ándalus",
     year: 1090,
     image: "images/cards/almoravidesB.png",
-    imageBack: "images/cards/almoravidesA.png",
+    imageBack: "images/cards/almoravidesA.png"
   },
   {
     year: -290000000,
     name: "Se forma la peninsula Iberica",
     image: "images/cards/inicioB.png",
-    imageBack: "images/cards/inicioA.png",
+    imageBack: "images/cards/inicioA.png"
   },
   {
     year: -1200000,
     name: "Primeros Hominidos En la Peninsula Iberica",
     image: "images/cards/homoB.png",
-    imageBack: "images/cards/homoA.png",
+    imageBack: "images/cards/homoA.png"
   },
   {
     year: -18000,
     name: "Primeras pinturas de altamira",
     image: "images/cards/PinturasB.png",
-    imageBack: "images/cards/PinturasA.png",
+    imageBack: "images/cards/PinturasA.png"
   },
   {
     year: -2000,
     name: "Primeros asentamientos Iberos",
     image: "images/cards/IberosB.png",
-    imageBack: "images/cards/IberosA.png",
+    imageBack: "images/cards/IberosA.png"
   },
   {
     year: -1100,
     name: "Los Fenicios Fundan Gadir",
     image: "images/cards/GadirB.png",
-    imageBack: "images/cards/GadirA.png",
+    imageBack: "images/cards/GadirA.png"
   },
   {
     year: -900,
     name: "Los Celtas entran en la peninsula",
     image: "images/cards/CeltasB.png",
-    imageBack: "images/cards/CeltasA.png",
+    imageBack: "images/cards/CeltasA.png"
   },
   {
     year: -654,
     name: "Los Cartagineses fundan Ebusus (ibiza)",
     image: "images/cards/ibizaB.png",
-    imageBack: "images/cards/ibizaA.png",
+    imageBack: "images/cards/ibizaA.png"
   },
   {
     year: -600,
     name: "Los Griegos fundan Ampurias",
     image: "images/cards/AmpuriasB.png",
-    imageBack: "images/cards/AmpuriasA.png",
+    imageBack: "images/cards/AmpuriasA.png"
   },
   {
     year: -264,
     name: "Guerras Punicas (inicio)",
     image: "images/cards/PunicasB.png",
-    imageBack: "images/cards/PunicasA.png",
+    imageBack: "images/cards/PunicasA.png"
   },
   {
     year: -218,
     name: "Destrucción de Sagunto (entran los romanos)",
     image: "images/cards/SaguntoB.png",
-    imageBack: "images/cards/SaguntoA.png",
+    imageBack: "images/cards/SaguntoA.png"
   },
   {
     year: -209,
     name: "Roma comienza la conquista de la Peninsula",
     image: "images/cards/RomaB.png",
-    imageBack: "images/cards/RomaA.png",
+    imageBack: "images/cards/RomaA.png"
   },
   //17
   {
     year: 1986,
     name: "España entra en la OTAN",
     image: "images/cards/otanB.png",
-    imageBack: "images/cards/otanA.png",
+    imageBack: "images/cards/otanA.png"
   },
   {
     year: 1987,
     name: "Atentado de ETA en Hipercor",
     image: "images/cards/hipercorB.png",
-    imageBack: "images/cards/hipercorA.png",
+    imageBack: "images/cards/hipercorA.png"
   },
   {
     year: 1992,
     name: "Olimpiadas de Barcelona y EXPO en Sevilla",
     image: "images/cards/olimpB.png",
-    imageBack: "images/cards/olimpA.png",
+    imageBack: "images/cards/olimpA.png"
   },
   {
     year: 2002,
     name: "Entra en vigor el EURO",
     image: "images/cards/euroB.png",
-    imageBack: "images/cards/euroA.png",
+    imageBack: "images/cards/euroA.png"
   },
   {
     year: 2004,
     name: "Atentado 11 Marzo en Atocha",
     image: "images/cards/atochaB.png",
-    imageBack: "images/cards/atochaA.png",
+    imageBack: "images/cards/atochaA.png"
   },
   {
     year: 2008,
     name: "Crisis Economica",
     image: "images/cards/crisisB.png",
-    imageBack: "images/cards/crisisA.png",
+    imageBack: "images/cards/crisisA.png"
   },
   //23
   {
     year: 1931,
     name: "2 Republica española",
     image: "images/cards/segrepB.png",
-    imageBack: "images/cards/segrepA.png",
+    imageBack: "images/cards/segrepA.png"
   },
   {
     year: 1936,
     name: "Guerra civil española",
     image: "images/cards/civilB.png",
-    imageBack: "images/cards/civilA.png",
+    imageBack: "images/cards/civilA.png"
   },
   {
     year: 1937,
     name: "Bombardeo de Guernica",
     image: "images/cards/guernicaB.png",
-    imageBack: "images/cards/guernicaA.png",
+    imageBack: "images/cards/guernicaA.png"
   },
   {
     year: 1939,
     name: "Comienza dictadura de franco",
     image: "images/cards/francoB.png",
-    imageBack: "images/cards/francoA.png",
+    imageBack: "images/cards/francoA.png"
   },
   {
     year: 1941,
     name: "Creacion de la RENFE",
     image: "images/cards/renfeB.png",
-    imageBack: "images/cards/renfeA.png",
+    imageBack: "images/cards/renfeA.png"
   },
   {
     year: 1955,
     name: "España ingresa en la ONU",
     image: "images/cards/onuB.png",
-    imageBack: "images/cards/onuA.png",
+    imageBack: "images/cards/onuA.png"
   },
   {
     year: 1959,
     name: "Creacion de ETA",
     image: "images/cards/etaB.png",
-    imageBack: "images/cards/etaA.png",
+    imageBack: "images/cards/etaA.png"
   },
   //30
   {
     year: 1480,
     name: "Creación de la Inquisición",
     image: "images/cards/inquiB.png",
-    imageBack: "images/cards/inquiA.png",
+    imageBack: "images/cards/inquiA.png"
   },
   {
     year: 1492,
     name: "Cristobal colon, el nuevo mundo",
     image: "images/cards/colonB.png",
-    imageBack: "images/cards/colonA.png",
+    imageBack: "images/cards/colonA.png"
   },
   {
     year: 1571,
     name: "Batalla de Lepanto",
     image: "images/cards/lepB.png",
-    imageBack: "images/cards/lepA.png",
+    imageBack: "images/cards/lepA.png"
   },
   {
     year: 1557,
     name: "Batalla de San quintin",
     image: "images/cards/quintinB.png",
-    imageBack: "images/cards/quintinA.png",
+    imageBack: "images/cards/quintinA.png"
   },
   {
     year: 1588,
     name: "Destruccion de la Armada Invencible",
     image: "images/cards/invB.png",
-    imageBack: "images/cards/invA.png",
+    imageBack: "images/cards/invA.png"
   },
   {
     year: 1099,
     name: "Muere El Cid Campeador",
     image: "images/cards/cidB.png",
-    imageBack: "images/cards/cidA.png",
+    imageBack: "images/cards/cidA.png"
   },
   {
     year: 1713,
     name: "Se crea la RAE",
     image: "images/cards/raeB.png",
-    imageBack: "images/cards/raeA.png",
+    imageBack: "images/cards/raeA.png"
   },
   {
     year: 1741,
     name: "Sitio de Cartagena de indias",
     image: "images/cards/sitioB.png",
-    imageBack: "images/cards/sitioA.png",
+    imageBack: "images/cards/sitioA.png"
   },
   {
     year: 1805,
     name: "Batalla de Trafalgar",
     image: "images/cards/trafB.png",
-    imageBack: "images/cards/trafA.png",
+    imageBack: "images/cards/trafA.png"
   },
   {
     year: 1812,
     name: "Las Cortes de Cadiz 1ºConstitucion",
     image: "images/cards/cadizB.png",
-    imageBack: "images/cards/cadizA.png",
+    imageBack: "images/cards/cadizA.png"
   },
   {
     year: 1873,
@@ -269,7 +269,7 @@ export let cards = [
   {
     year: 1978,
     name: "Referendum constitucional",
-    image: "images/cards/refemB.png",
-    imageBack: "images/cards/refemA.png",
+    image: "images/cards/referemB.png",
+    imageBack: "images/cards/referemA.png",
   },
 ];
