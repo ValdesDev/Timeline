c863cfbc9449da0bbd061036cecef868ce2461ce
JoseLValdes <jolvaldgo@gmail.com>
2021-11-04 17:37:41 +0100

    image name change

--- scripts/cards.js
+++ scripts/cards.js
@@ -20,8 +20,8 @@ export let cards = [
   {
     name: "Hispania se convierte en Al Ándalus",
     year: 716,
-    image: "images/cards/Al anB.png",
-    imageBack: "images/cards/Al anA.png",
+    image: "images/cards/AlanB.png",
+    imageBack: "images/cards/AlanA.png",
   },
   {
     name: "Comienza el Ducado de Alba",
@@ -105,172 +105,171 @@ export let cards = [
   {
     year: 1986,
     name: "España entra en la OTAN",
-    image:"images/cards/otanB.png",
-    imageBack:"images/cards/otanA.png"
+    image: "images/cards/otanB.png",
+    imageBack: "images/cards/otanA.png",
   },
   {
     year: 1987,
     name: "Atentado de ETA en Hipercor",
-    image:"images/cards/hipercorB.png",
-    imageBack:"images/cards/hipercorA.png"
+    image: "images/cards/hipercorB.png",
+    imageBack: "images/cards/hipercorA.png",
   },
   {
     year: 1992,
     name: "Olimpiadas de Barcelona y EXPO en Sevilla",
-    image:"images/cards/olimpB.png",
-    imageBack:"images/cards/olimpA.png"
+    image: "images/cards/olimpB.png",
+    imageBack: "images/cards/olimpA.png",
   },
   {
     year: 2002,
     name: "Entra en vigor el EURO",
-    image:"images/cards/euroB.png",
-    imageBack:"images/cards/euroA.png"
+    image: "images/cards/euroB.png",
+    imageBack: "images/cards/euroA.png",
   },
   {
     year: 2004,
     name: "Atentado 11 Marzo en Atocha",
-    image:"images/cards/atochaB.png",
-    imageBack:"images/cards/atochaA.png",
+    image: "images/cards/atochaB.png",
+    imageBack: "images/cards/atochaA.png",
   },
   {
     year: 2008,
     name: "Crisis Economica",
-    image:"images/cards/crisisB.png",
-    imageBack:"images/cards/crisisA.png"
+    image: "images/cards/crisisB.png",
+    imageBack: "images/cards/crisisA.png",
   },
   //23
   {
     year: 1931,
     name: "2 Republica española",
-    image:"images/cards/segrepB.png",
-    imageBack:"images/cards/segrepA.png",
+    image: "images/cards/segrepB.png",
+    imageBack: "images/cards/segrepA.png",
   },
   {
     year: 1936,
     name: "Guerra civil española",
-    image:"images/cards/civilB.png",
-    imageBack:"images/cards/civilA.png",
+    image: "images/cards/civilB.png",
+    imageBack: "images/cards/civilA.png",
   },
   {
     year: 1937,
     name: "Bombardeo de Guernica",
-    image:"images/cards/guernicaB.png",
-    imageBack:"images/cards/guernicaA.png"
+    image: "images/cards/guernicaB.png",
+    imageBack: "images/cards/guernicaA.png",
   },
   {
     year: 1939,
     name: "Comienza dictadura de franco",
-    image:"images/cards/francoB.png",
-    imageBack:"images/cards/francoA.png"
+    image: "images/cards/francoB.png",
+    imageBack: "images/cards/francoA.png",
   },
   {
     year: 1941,
     name: "Creacion de la RENFE",
-    image:"images/cards/renfeB.png",
-    imageBack:"images/cards/renfeA.png",
+    image: "images/cards/renfeB.png",
+    imageBack: "images/cards/renfeA.png",
   },
   {
     year: 1955,
     name: "España ingresa en la ONU",
-    image:"images/cards/onuB.png",
-    imageBack:"images/cards/onuA.png",
+    image: "images/cards/onuB.png",
+    imageBack: "images/cards/onuA.png",
   },
   {
     year: 1959,
     name: "Creacion de ETA",
-    image:"images/cards/etaB.png",
-    imageBack:"images/cards/etaA.png",
+    image: "images/cards/etaB.png",
+    imageBack: "images/cards/etaA.png",
   },
   //30
   {
     year: 1480,
     name: "Creación de la Inquisición",
-    image:"images/cards/inquiB.png",
-    imageBack:"images/cards/inquiA.png"
+    image: "images/cards/inquiB.png",
+    imageBack: "images/cards/inquiA.png",
   },
   {
     year: 1492,
     name: "Cristobal colon, el nuevo mundo",
-    image:"images/cards/colonB.png",
-    imageBack:"images/cards/colonA.png",
+    image: "images/cards/colonB.png",
+    imageBack: "images/cards/colonA.png",
   },
   {
     year: 1571,
     name: "Batalla de Lepanto",
-    image:"images/cards/lepB.png",
-    imageBack:"images/cards/lepA.png"
+    image: "images/cards/lepB.png",
+    imageBack: "images/cards/lepA.png",
   },
   {
     year: 1557,
     name: "Batalla de San quintin",
-    image:"images/cards/quintinB.png",
-    imageBack:"images/cards/quintinA.png",
+    image: "images/cards/quintinB.png",
+    imageBack: "images/cards/quintinA.png",
   },
   {
     year: 1588,
     name: "Destruccion de la Armada Invencible",
-    image:"images/cards/invB.png",
-    imageBack:"images/cards/invA.png",
+    image: "images/cards/invB.png",
+    imageBack: "images/cards/invA.png",
   },
   {
     year: 1099,
     name: "Muere El Cid Campeador",
-    image:"images/cards/cidB.png",
-    imageBack:"images/cards/cidA.png",
+    image: "images/cards/cidB.png",
+    imageBack: "images/cards/cidA.png",
   },
   {
     year: 1713,
     name: "Se crea la RAE",
-    image:"images/cards/raeB.png",
-    imageBack:"images/cards/raeA.png",
+    image: "images/cards/raeB.png",
+    imageBack: "images/cards/raeA.png",
   },
   {
     year: 1741,
     name: "Sitio de Cartagena de indias",
-    image:"images/cards/sitioB.png",
-    imageBack:"images/cards/sitioA.png",
+    image: "images/cards/sitioB.png",
+    imageBack: "images/cards/sitioA.png",
   },
   {
     year: 1805,
     name: "Batalla de Trafalgar",
-    image:"images/cards/trafB.png",
-    imageBack:"images/cards/trafA.png",
-
+    image: "images/cards/trafB.png",
+    imageBack: "images/cards/trafA.png",
   },
   {
     year: 1812,
     name: "Las Cortes de Cadiz 1ºConstitucion",
-    image:"images/cards/cadizB.png",
-    imageBack:"images/cards/cadizA.png",
+    image: "images/cards/cadizB.png",
+    imageBack: "images/cards/cadizA.png",
   },
   {
     year: 1873,
     name: "El Cantón de Cartagena",
-    image:"images/cards/cantonB.png",
-    imageBack:"images/cards/cantonA.png",
-    
+    image: "images/cards/cantonB.png",
+    imageBack: "images/cards/cantonA.png",
   },
   {
     year: 1898,
     name: "Guerra con Estados Unidos",
-    image:"images/cards/ussB.png",
-    imageBack:"images/cards/ussA.png",
+    image: "images/cards/ussB.png",
+    imageBack: "images/cards/ussA.png",
   },
   {
     year: 1909,
     name: "La Semana Trágica",
-    image:"images/cards/tragB.png",
-    imageBack:"images/cards/tragA.png",
+    image: "images/cards/tragB.png",
+    imageBack: "images/cards/tragA.png",
   },
   {
     year: 1921,
     name: "Desastre de Annual (Marruecos)",
-    image:"images/cards/annualB.png",
-    imageBack:"images/cards/annualA.png",
-  },{
+    image: "images/cards/annualB.png",
+    imageBack: "images/cards/annualA.png",
+  },
+  {
     year: 1978,
     name: "Referendum constitucional",
-    image:"images/cards/refemB.png",
-    imageBack:"images/cards/refemA.png",
-  }
+    image: "images/cards/refemB.png",
+    imageBack: "images/cards/refemA.png",
+  },
 ];
